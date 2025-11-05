303a76a46ff6df74605ab07d25d100aca33e5828
Bruno de Fraga <brunofragaa97@gmail.com>
2025-11-05 17:33:48 -0300

    alterado nos outros microservicos para utilizar agora um record no UserDTO

--- servicos/feedbackservice/src/main/java/com/webservice/feedbackservice/sistema/dto/UserDTO.java
+++ servicos/feedbackservice/src/main/java/com/webservice/feedbackservice/sistema/dto/UserDTO.java
@@ -1,24 +1,13 @@
 package com.webservice.feedbackservice.sistema.dto;
 
+
 import com.webservice.feedbackservice.sistema.enums.UserRole;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserDTO {
-    private Long id;
-    private String nome;
-    private String userName;
-    private String email;
-    private UserRole userRole;
-
-
-    public String getUserName(){
-        return userName.toUpperCase();
-    }
+public record UserDTOrecord(
+        Long id,
+        String nome,
+        String userName,
+        String email,
+        UserRole userRole
+) {
 }
-
-
